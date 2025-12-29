37a6e6866dd7e5b4fbd1467ecffe8e52f97d3de1
opficdev <opficdev@gmail.com>
2025-12-29 23:43:39 +0900

    style: 이름에서 'Impl' 제거

--- DevLog/App/Assembler/DomainAssembler.swift
+++ DevLog/App/Assembler/DomainAssembler.swift
@@ -32,15 +32,15 @@ final class DomainAssembler: Assembler {
             FetchPinnedTodosUseCaseImpl(container.resolve(TodoRepository.self))
         }
 
-        container.register(SignInUseCase.self, name: "SignInWithAppleUseCaseImpl") {
+        container.register(SignInUseCase.self, name: "SignInWithAppleUseCase") {
             SignInWithAppleUseCaseImpl(container.resolve(AuthenticationRepository.self))
         }
 
-        container.register(SignInUseCase.self, name: "SignInWithGithubUseCaseImpl") {
+        container.register(SignInUseCase.self, name: "SignInWithGithubUseCase") {
             SignInWithGithubUseCaseImpl(container.resolve(AuthenticationRepository.self))
         }
 
-        container.register(SignInUseCase.self, name: "SignInWithGoogleUseCaseImpl") {
+        container.register(SignInUseCase.self, name: "SignInWithGoogleUseCase") {
             SignInWithGoogleUseCaseImpl(container.resolve(AuthenticationRepository.self))
         }
 
