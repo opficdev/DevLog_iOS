4f33b9e50ba804f5c7a282f79db6aff1ec355699
opficdev <opficdev@gmail.com>
2025-12-30 14:13:06 +0900

    fix: UI 요소가 MainActor에서 호출되지 않는 현상 해결

--- DevLog/App/Assembler/InfraAssembler.swift
+++ DevLog/App/Assembler/InfraAssembler.swift
@@ -5,7 +5,8 @@
 //  Created by 최윤진 on 12/7/25.
 //
 
-final class InfraAssembler: Assembler {
+final class InfraAssembler: @MainActor Assembler {
+    @MainActor
     func assemble(_ container: any DIContainer) {
         container.register(
             AuthenticationService.self,
