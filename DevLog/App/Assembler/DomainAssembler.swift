b2003ca930841db189dbef401ec892a6e75493da
opficdev <opficdev@gmail.com>
2026-01-24 12:03:47 +0900

    refactor: Data 레이어 의존성 주입 분리

--- DevLog/App/Assembler/DomainAssembler.swift
+++ DevLog/App/Assembler/DomainAssembler.swift
@@ -7,47 +7,6 @@
 
 final class DomainAssembler: Assembler {
     func assemble(_ container: DIContainer) {
-        registerRepositories(container)
-        registerUseCases(container)
-    }
-
-    func registerRepositories(_ container: any DIContainer) {
-        container.register(AuthenticationRepository.self) {
-            AuthenticationRepositoryImpl(
-                authService: container.resolve(AuthService.self),
-                appleAuthService: container.resolve(
-                    AuthenticationService.self,
-                    name: "AppleAuthenticationService"
-                ),
-                githubAuthService: container.resolve(
-                    AuthenticationService.self,
-                    name: "GithubAuthenticationService"
-                ),
-                googleAuthService: container.resolve(
-                    AuthenticationService.self,
-                    name: "GoogleAuthenticationService"
-                ),
-                userService: container.resolve(UserService.self)
-            )
-        }
-
-        container.register(TodoRepository.self) {
-            TodoRepositoryImpl(
-                authService: container.resolve(AuthService.self),
-                todoService: container.resolve(TodoService.self)
-            )
-        }
-
-        container.register(AuthSessionRepository.self) {
-            AuthSessionRepositoryImpl(authService: container.resolve(AuthService.self))
-        }
-
-        container.register(UserDataRepository.self) {
-            UserDataRepositoryImpl(userService: container.resolve(UserService.self))
-        }
-    }
-
-    func registerUseCases(_ container: any DIContainer) {
         container.register(FetchPinnedTodosUseCase.self) {
             FetchPinnedTodosUseCaseImpl(container.resolve(TodoRepository.self))
         }
