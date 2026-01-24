bce0925392e7b47ed38433e18d6872b523f7f6dc
opficdev <opficdev@gmail.com>
2026-01-24 12:03:47 +0900

    refactor: Data 레이어 의존성 주입 분리

--- DevLog/App/Assembler/DataAssembler.swift
+++ DevLog/App/Assembler/DataAssembler.swift
@@ -7,6 +7,38 @@
 
 final class DataAssembler: Assembler {
     func assemble(_ container: any DIContainer) {
-        
+        container.register(AuthenticationRepository.self) {
+            AuthenticationRepositoryImpl(
+                authService: container.resolve(AuthService.self),
+                appleAuthService: container.resolve(
+                    AuthenticationService.self,
+                    name: "AppleAuthenticationService"
+                ),
+                githubAuthService: container.resolve(
+                    AuthenticationService.self,
+                    name: "GithubAuthenticationService"
+                ),
+                googleAuthService: container.resolve(
+                    AuthenticationService.self,
+                    name: "GoogleAuthenticationService"
+                ),
+                userService: container.resolve(UserService.self)
+            )
+        }
+
+        container.register(TodoRepository.self) {
+            TodoRepositoryImpl(
+                authService: container.resolve(AuthService.self),
+                todoService: container.resolve(TodoService.self)
+            )
+        }
+
+        container.register(AuthSessionRepository.self) {
+            AuthSessionRepositoryImpl(authService: container.resolve(AuthService.self))
+        }
+
+        container.register(UserDataRepository.self) {
+            UserDataRepositoryImpl(userService: container.resolve(UserService.self))
+        }
     }
 }
