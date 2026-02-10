891cd97f54cacd96e8633c5be35e0d1a0ef9535f
opficdev <opficdev@gmail.com>
2026-02-10 11:54:02 +0900

    refactor: 도메인 모델 재정의

--- DevLog/Infra/DTO/PushNotification.swift
+++ DevLog/Infra/DTO/PushNotification.swift
@@ -6,41 +6,12 @@
 //
 
 import Foundation
-import FirebaseFirestore
 
-struct PushNotification: Codable, Identifiable {
-    @DocumentID var id: String?
-    let title: String   //  알림 제목
-    let body: String    //  알림 내용
-    let receivedDate: Date  //  알림 수신 날짜
-    var isRead: Bool    //  알림 읽음 여부
-    let todoId: String //  Todo ID
-    
-    init(from: QueryDocumentSnapshot) {
-        self.id = from.documentID
-        self.title = from["title"] as? String ?? ""
-        self.body = from["body"] as? String ?? ""
-        self.receivedDate = (from["receivedDate"] as? Timestamp)?.dateValue() ?? Date()
-        self.isRead = from["isRead"] as? Bool ?? false
-        self.todoId = from["todoId"] as? String ?? ""
-    }
-    
-    init(id: String? = nil, title: String, body: String, receivedDate: Date, isRead: Bool, todoId: String) {
-        self.id = id
-        self.title = title
-        self.body = body
-        self.receivedDate = receivedDate
-        self.isRead = isRead
-        self.todoId = todoId
-    }
-    
-    func toDictionary() -> [String: Any] {
-        return [
-            "title": title,
-            "body": body,
-            "receivedDate": Timestamp(date: receivedDate),
-            "isRead": isRead,
-            "todoId": todoId
-        ]
-    }
+struct PushNotification: Identifiable {
+    let id: String
+    let title: String
+    let body: String
+    let receivedAt: Date
+    var isRead: Bool
+    let todoID: String
 }
