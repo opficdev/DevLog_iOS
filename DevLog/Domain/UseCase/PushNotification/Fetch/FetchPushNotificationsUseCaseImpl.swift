d84e32248513d993e3225430cfd876087e73f7a3
opficdev <opficdev@gmail.com>
2026-02-10 12:01:30 +0900

    feat: 유즈케이스 구현

---
+++ DevLog/Domain/UseCase/PushNotification/Fetch/FetchPushNotificationsUseCaseImpl.swift
@@ -0,0 +1,18 @@
+//
+//  FetchPushNotificationsUseCaseImpl.swift
+//  DevLog
+//
+//  Created by 최윤진 on 2/10/26.
+//
+
+final class FetchPushNotificationsUseCaseImpl: FetchPushNotificationsUseCase {
+    private let repository: PushNotificationRepository
+
+    init(_ repository: PushNotificationRepository) {
+        self.repository = repository
+    }
+
+    func execute() async throws -> [PushNotification] {
+        try await repository.requestNotifications()
+    }
+}
