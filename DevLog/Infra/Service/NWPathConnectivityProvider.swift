b5dc1f0e24f37dcb79f0718896c7629554e0d3f1
opficdev <opficdev@gmail.com>
2026-02-12 17:18:58 +0900

    refactor: AsyncStream을 CurrentValueSubject 기반 Publisher을 방출하도록 개선

--- DevLog/Infra/Service/NWPathConnectivityProvider.swift
+++ DevLog/Infra/Service/NWPathConnectivityProvider.swift
@@ -5,58 +5,35 @@
 //  Created by 최윤진 on 11/2/25.
 //
 
-import Foundation
 import Network
+import Combine
 
 final class NWPathConnectivityProvider {
     private let networkPathMonitor = NWPathMonitor()
     private let monitoringQueue = DispatchQueue(label: "NWPathConnectivityProviderQueue")
-
-    private var isConnectedValue: Bool
-    private var connectivityContinuations: [UUID: AsyncStream<Bool>.Continuation] = [:]
+    private let isConnectedSubject = CurrentValueSubject<Bool, Never>(false)
+    
+    var isConnectedPublisher: AnyPublisher<Bool, Never> {
+        isConnectedSubject.eraseToAnyPublisher()
+    }
+    
+    var isConnected: Bool {
+        isConnectedSubject.value
+    }
 
     init() {
-        self.isConnectedValue = (networkPathMonitor.currentPath.status == .satisfied)
+        let initialStatus = networkPathMonitor.currentPath.status == .satisfied
+        isConnectedSubject.send(initialStatus)
 
-        self.networkPathMonitor.pathUpdateHandler = { [weak self] path in
+        networkPathMonitor.pathUpdateHandler = { [weak self] path in
             let connected = (path.status == .satisfied)
-            Task { @MainActor in
-                self?.handlePathStatusChange(isConnected: connected)
-            }
+            self?.isConnectedSubject.send(connected)
         }
-        self.networkPathMonitor.start(queue: monitoringQueue)
+        networkPathMonitor.start(queue: monitoringQueue)
     }
 
     deinit {
-        self.networkPathMonitor.cancel()
-        self.connectivityContinuations.values.forEach { $0.finish() }
-        self.connectivityContinuations.removeAll()
-    }
-
-    var isConnected: Bool {
-        self.isConnectedValue
-    }
-
-    func connectivityStream() -> AsyncStream<Bool> {
-        let identifier = UUID()
-        return AsyncStream(bufferingPolicy: .bufferingNewest(1)) { [weak self] continuation in
-            guard let self else { return }
-            self.connectivityContinuations[identifier] = continuation
-            continuation.yield(self.isConnectedValue)
-
-            continuation.onTermination = { [weak self] _ in
-                Task { @MainActor in
-                    self?.connectivityContinuations.removeValue(forKey: identifier)
-                }
-            }
-        }
-    }
-
-    private func handlePathStatusChange(isConnected: Bool) {
-        guard isConnected != self.isConnectedValue else { return }
-        self.isConnectedValue = isConnected
-        for continuation in self.connectivityContinuations.values {
-            continuation.yield(isConnected)
-        }
+        networkPathMonitor.cancel()
+        isConnectedSubject.send(completion: .finished)
     }
 }
