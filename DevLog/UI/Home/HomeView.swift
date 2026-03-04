1404cff90c1e67d3e6b8ba9fa078d89ea364bc2a
최윤진 <opficdev@gmail.com>
2026-03-04 21:28:39 +0900

    style: autocorrectionDisabled 위치 이동 (#182)

--- DevLog/UI/Home/HomeView.swift
+++ DevLog/UI/Home/HomeView.swift
@@ -143,7 +143,6 @@ struct HomeView: View {
             )
             .textInputAutocapitalization(.never)
             .keyboardType(.URL)
-            .autocorrectionDisabled()
             Button("추가") {
                 viewModel.send(.addWebPage)
             }
