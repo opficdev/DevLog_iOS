1f53f669c00fc2ffe43289bc210757ce20119e9e
최윤진 <opficdev@gmail.com>
2026-03-05 19:49:33 +0900

    [#191] 최초 로그인 시 자동으로 로그아웃 되는 현상을 해결한다 (#192)
    * fix: 앱이 처음 실행되는 플래그가 삭제되어 자동 로그아웃이 되어 해결
    
    * refactor: 타입 안정성 개선

--- DevLog/Storage/Persistence/UserDefaultsStore.swift
+++ DevLog/Storage/Persistence/UserDefaultsStore.swift
@@ -40,6 +40,10 @@ final class UserDefaultsStore {
 
     func removeAll() {
         guard let bundleIdentifier = Bundle.main.bundleIdentifier else { return }
+        let firstLaunch = userDefaults.object(forKey: "isFirstLaunch")
         userDefaults.removePersistentDomain(forName: bundleIdentifier)
+        if let value = firstLaunch as? Bool {
+            userDefaults.set(value, forKey: "isFirstLaunch")
+        }
     }
 }
