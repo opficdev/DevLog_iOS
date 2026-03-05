6a479f5ced3d2d3785cd76a0c85a272c9123c2da
최윤진 <opficdev@gmail.com>
2026-03-05 22:51:40 +0900

    [#190] adaptiveButtonStyle을 개선한다 (#195)
    * ui: iOS 18 이하 라이트모드에서의 가독성 개선
    
    * ui: glassEffect로 인해 뷰 주변에 블러 형태가 보이는 현상 방지
    
    * style: 불필요 괄호 제거

--- DevLog/UI/Extension/View+.swift
+++ DevLog/UI/Extension/View+.swift
@@ -101,6 +101,7 @@ extension View {
             self.foregroundStyle(Color(.label))
                 .padding(8)
                 .glassEffect(.regular.tint(color), in: shape)
+                .clipShape(shape)
         } else {
             self.foregroundStyle(Color(.label))
                 .padding(8)
@@ -108,7 +109,7 @@ extension View {
                     Group {
                         if color == .clear {
                             shape
-                                .fill(.ultraThinMaterial)
+                                .fill(Color(.systemGray5))
                         } else {
                             shape
                                 .fill(color)
