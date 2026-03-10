ee7715e0bfe2d0f2a9170496eb935f2626321cb8
최윤진 <opficdev@gmail.com>
2026-03-10 17:17:50 +0900

    [#242] 애플 계정의 링크 끊기가 실패하는 이슈를 해결한다 (#244)
    * fix: getAppleConfiguration()을 통해 애플 관련 키를 가져오도록 수정하여 해결
    
    * feat: 에러가 날 수 있는 코드의 상단에 Logger 추가
    
    * style: 인덴트 개선
    
    * style: 에러 발생 시 Logger 뜨도록 추가

--- DevLog/Infra/Service/SocialLogin/GoogleAuthenticationService.swift
+++ DevLog/Infra/Service/SocialLogin/GoogleAuthenticationService.swift
@@ -119,12 +119,17 @@ final class GoogleAuthenticationService: AuthenticationService {
     }
 
     func unlink(_ uid: String) async throws {
-        logger.info("Starting Google disconnect for unlink. uid: \(uid)")
-        GIDSignIn.sharedInstance.signOut()
-        try await GIDSignIn.sharedInstance.disconnect()
+        do {
+            logger.info("Starting Google disconnect for unlink. uid: \(uid)")
+            GIDSignIn.sharedInstance.signOut()
+            try await GIDSignIn.sharedInstance.disconnect()
 
-        logger.info("Starting Firebase Google provider unlink. uid: \(uid)")
-        _ = try await user?.unlink(fromProvider: AuthProviderID.google.rawValue)
+            logger.info("Starting Firebase Google provider unlink. uid: \(uid)")
+            _ = try await user?.unlink(fromProvider: AuthProviderID.google.rawValue)
+        } catch {
+            logger.error("Failed to unlink Google account", error: error)
+            throw error
+        }
     }
 
 }
