195c2eb057c3a52d87b332abd5dde790acc9e434
opficdev <opficdev@gmail.com>
2026-05-15 14:46:54 +0900

    chore: DataProtocol의 불필요한 Domain import 정리

--- Application/DevLogData/Sources/Protocol/NWPathConnectivityProvider.swift
+++ Application/DevLogData/Sources/Protocol/NWPathConnectivityProvider.swift
@@ -7,7 +7,6 @@
 
 import Combine
 import Foundation
-import DevLogDomain
 import DevLogDataDTO
 
 public protocol NWPathConnectivityProvider {
--- Application/DevLogData/Sources/Protocol/WebPageService.swift
+++ Application/DevLogData/Sources/Protocol/WebPageService.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import DevLogDomain
 import DevLogDataDTO
 
 public protocol WebPageService {
