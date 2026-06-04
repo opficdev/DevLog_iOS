5e248dc88c188e0f5e31297ee24d385297cd7619
opficdev <162981733+opficdev@users.noreply.github.com>
2026-06-04 09:48:29 +0900

    refactor: 프로토콜에 Sendable 추가

--- Application/DevLogData/Sources/Protocol/WebPageImageStore.swift
+++ Application/DevLogData/Sources/Protocol/WebPageImageStore.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public protocol WebPageImageStore {
+public protocol WebPageImageStore: Sendable {
     func cachedImageURL(for url: URL) async throws -> URL
     func saveImage(_ data: Data, for url: URL) async throws -> URL
     func dirSizeInBytes() async -> Int64
