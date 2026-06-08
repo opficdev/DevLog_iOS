18cc91cbf79f0f1548ee2a9a13f5dc381d536a11
opficdev <162981733+opficdev@users.noreply.github.com>
2026-06-08 19:50:20 +0900

    chore: 기본 설정으로 롤백

--- Tuist/ProjectDescriptionHelpers/Project+Settings.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Settings.swift
@@ -16,7 +16,6 @@ public extension Settings {
             "CURRENT_PROJECT_VERSION": "1",
             "INFOPLIST_KEY_CFBundleShortVersionString": "$(MARKETING_VERSION)",
             "INFOPLIST_KEY_CFBundleVersion": "$(CURRENT_PROJECT_VERSION)",
-            "SWIFT_STRICT_CONCURRENCY": "complete",
             "SWIFT_VERSION": "5.0",
             "TARGETED_DEVICE_FAMILY": "1,2",
         ]
