b80bc9277edd77a06cf8dbdab23e0d7a28182e37
opficdev <162981733+opficdev@users.noreply.github.com>
2026-06-09 09:48:53 +0900

    refactor: 메모리 캐시 저장 방식 개선

--- Application/DevLogPersistence/Sources/Cache/MemoryCacheStoreImpl.swift
+++ Application/DevLogPersistence/Sources/Cache/MemoryCacheStoreImpl.swift
@@ -9,30 +9,25 @@ import Foundation
 import DevLogData
 
 final class MemoryCacheStoreImpl: MemoryCacheStore {
-    private let queue = DispatchQueue(
-        label: "devlog.memory-cache-store",
-        qos: .utility
-    )
-    private var values = [String: Data]()
+    private let lock = NSLock()
+    private var values = [String: Any]()
 
     func value<T: Codable>(forKey key: String) -> T? {
-        queue.sync {
-            let decoder = JSONDecoder()
-            guard let data = self.values[key] else { return nil }
-            return try? decoder.decode(T.self, from: data)
-        }
+        lock.lock()
+        defer { lock.unlock() }
+
+        return values[key] as? T
     }
 
     func setValue<T: Codable>(_ value: T?, forKey key: String) {
-        queue.sync {
-            let encoder = JSONEncoder()
-            guard let value else {
-                self.values.removeValue(forKey: key)
-                return
-            }
+        lock.lock()
+        defer { lock.unlock() }
 
-            guard let data = try? encoder.encode(value) else { return }
-            self.values[key] = data
+        guard let value else {
+            values.removeValue(forKey: key)
+            return
         }
+
+        values[key] = value
     }
 }
