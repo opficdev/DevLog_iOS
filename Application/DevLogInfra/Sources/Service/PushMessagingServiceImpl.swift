892a13bdee2319f857e5722db6a389ff7798e2a1
opficdev <162981733+opficdev@users.noreply.github.com>
2026-06-13 15:16:46 +0900

    fix: FCMTokenSyncHandler에서 토큰 backfill 흐름을 처리하도록 수정

--- Application/DevLogInfra/Sources/Service/PushMessagingServiceImpl.swift
+++ Application/DevLogInfra/Sources/Service/PushMessagingServiceImpl.swift
@@ -8,6 +8,7 @@
 import Foundation
 import DevLogData
 import FirebaseMessaging
+import UserNotifications
 
 final class PushMessagingServiceImpl: NSObject, PushMessagingService {
     private weak var delegate: PushMessagingServiceDelegate?
@@ -20,10 +21,41 @@ final class PushMessagingServiceImpl: NSObject, PushMessagingService {
     func setAPNSToken(_ deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
     }
+
+    func isNotificationAuthorized() async -> Bool {
+        let settings = await UNUserNotificationCenter.current().notificationSettings()
+
+        switch settings.authorizationStatus {
+        case .authorized, .provisional, .ephemeral:
+            return true
+        case .denied, .notDetermined:
+            return false
+        @unknown default:
+            return false
+        }
+    }
+
+    func fetchFCMToken() async throws -> String? {
+        do {
+            return try await Messaging.messaging().token()
+        } catch {
+            if error.isMissingAPNSTokenForFCMToken {
+                return nil
+            }
+            throw error
+        }
+    }
 }
 
 extension PushMessagingServiceImpl: MessagingDelegate {
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         delegate?.pushMessagingService(self, didReceiveRegistrationToken: fcmToken)
+    }
+}
+
+private extension Error {
+    var isMissingAPNSTokenForFCMToken: Bool {
+        let nsError = self as NSError
+        return nsError.domain == "com.google.fcm" && nsError.code == 505
     }
 }
