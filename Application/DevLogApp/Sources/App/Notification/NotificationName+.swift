99da5e88f5488d3a0b4903bb7ed6e3a14fd25309
opficdev <162981733+opficdev@users.noreply.github.com>
2026-06-13 15:16:26 +0900

    fix: AppDelegate에서 FCM 동기화 트리거를 분리하도록 수정

--- Application/DevLogApp/Sources/App/Notification/NotificationName+.swift
+++ Application/DevLogApp/Sources/App/Notification/NotificationName+.swift
@@ -9,5 +9,8 @@ import Foundation
 
 extension Notification.Name {
     static let didRefreshFCMToken = Notification.Name("didRefreshFCMToken")
+    static let didReceiveAPNSToken = Notification.Name("didReceiveAPNSToken")
+    static let didRequestFCMTokenSync = Notification.Name("didRequestFCMTokenSync")
+    static let didRequestRemoteNotificationRegistration = Notification.Name("didRequestRemoteNotificationRegistration")
     static let didRequestUserTimeZoneSync = Notification.Name("didRequestUserTimeZoneSync")
 }
