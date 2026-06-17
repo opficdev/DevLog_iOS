3fbfb24e83e505dee0ce5433f811a95ef6aa075f
opficdev <162981733+opficdev@users.noreply.github.com>
2026-06-17 09:41:29 +0900

    refactor: sheetContent 분리

--- Application/DevLogPresentation/Sources/Root/RootView.swift
+++ Application/DevLogPresentation/Sources/Root/RootView.swift
@@ -66,27 +66,36 @@ public struct RootView: View {
         }
         .alert($store.scope(state: \.alert, action: \.alert))
         .sheet(item: $store.scope(state: \.sheet, action: \.sheet)) { sheetStore in
-            NavigationStack {
-                TodoDetailView(store: Store(
-                    initialState: TodoDetailFeature.State(todoId: sheetStore.todoId, showEditButton: false)
-                ) {
-                    TodoDetailFeature()
-                } withDependencies: {
-                    $0.fetchTodoByIdUseCase = container.resolve(FetchTodoByIdUseCase.self)
-                    $0.fetchReferenceItemsUseCase = container.resolve(FetchReferenceItemsUseCase.self)
-                })
-                .toolbar {
-                    ToolbarLeadingButton {
-                        sheetStore.send(.tapCloseButton)
-                    }
-                }
+            sheetContent(todoId: sheetStore.todoId) {
+                sheetStore.send(.tapCloseButton)
             }
-            .background(Color(.systemGroupedBackground))
-            .presentationDragIndicator(.visible)
         }
         .onReceive(pushNotificationTodoIdPublisher) { todoId in
             store.send(.presentTodoDetail(todoId))
             clearPushNotificationRoute()
         }
+    }
+
+    private func sheetContent(
+        todoId: String,
+        onClose: @escaping () -> Void
+    ) -> some View {
+        NavigationStack {
+            TodoDetailView(store: Store(
+                initialState: TodoDetailFeature.State(todoId: todoId, showEditButton: false)
+            ) {
+                TodoDetailFeature()
+            } withDependencies: {
+                $0.fetchTodoByIdUseCase = container.resolve(FetchTodoByIdUseCase.self)
+                $0.fetchReferenceItemsUseCase = container.resolve(FetchReferenceItemsUseCase.self)
+            })
+            .toolbar {
+                ToolbarLeadingButton {
+                    onClose()
+                }
+            }
+        }
+        .background(Color(.systemGroupedBackground))
+        .presentationDragIndicator(.visible)
     }
 }
