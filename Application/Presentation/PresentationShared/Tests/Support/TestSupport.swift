f78437e7b9503b6f526486965e4240b21925a0d1
opficdev <162981733+opficdev@users.noreply.github.com>
2026-07-06 18:29:23 +0900

    chore: Entry 테스트 support 중복 제거

--- Application/Presentation/PresentationShared/Tests/Support/TestSupport.swift
+++ Application/Presentation/PresentationShared/Tests/Support/TestSupport.swift
@@ -26,50 +26,6 @@ func waitUntil(
     }
 }
 
-final class SignInUseCaseSpy: SignInUseCase {
-    var error: Error?
-    var signedIn = true
-    var shouldSuspend = false
-    private(set) var calledProviders: [AuthProvider] = []
-    private(set) var successfulProviders = [AuthProvider]()
-    private var continuation: CheckedContinuation<Void, Never>?
-    private var shouldResume = false
-
-    func execute(_ provider: AuthProvider) async throws -> Bool {
-        calledProviders.append(provider)
-
-        if shouldSuspend {
-            await withCheckedContinuation { continuation in
-                if shouldResume {
-                    shouldResume = false
-                    continuation.resume()
-                } else {
-                    self.continuation = continuation
-                }
-            }
-        }
-
-        if let error {
-            throw error
-        }
-
-        if signedIn {
-            successfulProviders.append(provider)
-        }
-
-        return signedIn
-    }
-
-    func resume() {
-        guard let continuation else {
-            shouldResume = true
-            return
-        }
-        self.continuation = nil
-        continuation.resume()
-    }
-}
-
 final class FetchTodoCategoryPreferencesUseCaseSpy: FetchTodoCategoryPreferencesUseCase {
     var todoCategoryPreferences: [TodoCategoryPreference] = []
 
