431674663acdfe829d3d4a155f8397591fba3f38
최윤진 <opficdev@gmail.com>
2026-07-06 20:05:01 +0900

    [#697] Presentation 계열 테스트 지원 코드를 target-local로 정리한다 (#700)
    * refactor: NotificationTab의 target-local 정리
    
    * chore: Entry 테스트 support 중복 제거
    
    * refactor: HomeTab 테스트 support 중복 제거
    
    * refactor: ProfileTab 테스트 support 중복 제거
    
    * refactor: PresentationShared 테스트 support 범위 축소

---
+++ Application/Presentation/NotificationTab/Tests/PushNotification/PushNotificationListTestSpies.swift
@@ -0,0 +1,52 @@
+//
+//  PushNotificationListTestSpies.swift
+//  NotificationTabTests
+//
+//  Created by opfic on 7/6/26.
+//
+
+import Domain
+
+final class DeletePushNotificationUseCaseSpy: DeletePushNotificationUseCase {
+    private(set) var calledNotificationIds = [String]()
+
+    func execute(_ notificationID: String) async throws {
+        calledNotificationIds.append(notificationID)
+    }
+}
+
+final class UndoDeletePushNotificationUseCaseSpy: UndoDeletePushNotificationUseCase {
+    private(set) var calledNotificationIds = [String]()
+
+    func execute(_ notificationID: String) async throws {
+        calledNotificationIds.append(notificationID)
+    }
+}
+
+final class TogglePushNotificationReadUseCaseSpy: TogglePushNotificationReadUseCase {
+    private(set) var calledTodoIds = [String]()
+    var error: Error?
+
+    func execute(_ todoId: String) async throws {
+        calledTodoIds.append(todoId)
+        if let error {
+            throw error
+        }
+    }
+}
+
+final class FetchPushNotificationQueryUseCaseSpy: FetchPushNotificationQueryUseCase {
+    var pushNotificationQuery = PushNotificationQuery.default
+
+    func execute() -> PushNotificationQuery {
+        pushNotificationQuery
+    }
+}
+
+final class UpdatePushNotificationQueryUseCaseSpy: UpdatePushNotificationQueryUseCase {
+    private(set) var queries = [PushNotificationQuery]()
+
+    func execute(_ query: PushNotificationQuery) {
+        queries.append(query)
+    }
+}
