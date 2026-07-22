1815ff58e8fb3c7b1462d9f52af04854de741191
opficdev <opficdev@gmail.com>
2026-07-22 10:42:15 +0900

    fix: Xcode 직접 빌드의 미전송 Crashlytics 보고서 정리

--- Application/Infra/Sources/Service/FirebaseAppServiceImpl.swift
+++ Application/Infra/Sources/Service/FirebaseAppServiceImpl.swift
@@ -7,14 +7,40 @@
 
 import Data
 import FirebaseCore
+import FirebaseCrashlytics
+import Foundation
 
 final class FirebaseAppServiceImpl: FirebaseAppService {
+    private enum InfoKey {
+        static let crashlyticsCollectionEnabled = "FirebaseCrashlyticsCollectionEnabled"
+    }
+
     private static var isConfigured = false
 
     func configure() {
         guard !Self.isConfigured else { return }
 
         FirebaseApp.configure()
+
+        let crashlytics = Crashlytics.crashlytics()
+        let policy = FirebaseCrashlyticsCollectionPolicy(
+            infoDictionaryValue: Bundle.main.object(
+                forInfoDictionaryKey: InfoKey.crashlyticsCollectionEnabled
+            )
+        )
+        let actions = policy.actions(
+            currentCollectionEnabled: crashlytics.isCrashlyticsCollectionEnabled()
+        )
+
+        for action in actions {
+            switch action {
+            case .deleteUnsentReports:
+                crashlytics.deleteUnsentReports()
+            case let .setCollectionEnabled(isEnabled):
+                crashlytics.setCrashlyticsCollectionEnabled(isEnabled)
+            }
+        }
+
         Self.isConfigured = true
     }
 }
