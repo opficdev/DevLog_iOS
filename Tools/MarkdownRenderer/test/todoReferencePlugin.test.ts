918cfd7c1a3bb9a1977a98f5a32b1f112ea3792b
opficdev <opficdev@gmail.com>
2026-07-25 12:54:49 +0900

    fix: 들여쓴 Todo 참조 렌더링 복구

--- Tools/MarkdownRenderer/test/todoReferencePlugin.test.ts
+++ Tools/MarkdownRenderer/test/todoReferencePlugin.test.ts
@@ -36,6 +36,7 @@ test("Todo 참조 문법이 아닌 항목은 변경하지 않는다", () => {
 
 \`\`\`
 - refs #42
+    - refs #42
 \`\`\`
 `,
     {
@@ -49,7 +50,31 @@ test("Todo 참조 문법이 아닌 항목은 변경하지 않는다", () => {
   assert.match(html, /<li>ref #42<\/li>/);
   assert.match(html, /<li>refs 42<\/li>/);
   assert.match(html, /<code>- refs #42<\/code>/);
-  assert.match(html, /<code>- refs #42\n<\/code>/);
+  assert.match(html, /<code>- refs #42\n    - refs #42\n<\/code>/);
+});
+
+test("들여쓴 기존 Todo 참조도 전용 요소로 변환한다", () => {
+  const html = renderMarkdown(
+    `
+    before
+        - refs #42
+\t- refs #7
+    after
+`,
+    {
+      42: {
+        title: "연결된 Todo"
+      }
+    }
+  );
+
+  assert.match(html, /<code>before\n<\/code>/);
+  assert.match(
+    html,
+    /<button type="button" class="todo-reference" data-todo-reference-number="42">refs #42<\/button>/
+  );
+  assert.match(html, /<li>refs #7<\/li>/);
+  assert.match(html, /<code>after\n<\/code>/);
 });
 
 test("같은 Todo 참조가 반복되면 각각 전용 요소로 변환한다", () => {
