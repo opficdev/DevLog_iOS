4de2fbcb077e60459d8e7dc4a6952ead99fa7b5b
opficdev <opficdev@gmail.com>
2026-08-20 17:50:37 +0900

    feat: CollectionUI SwiftUI 연결 추가

---
+++ Libraries/CollectionUI/Sources/CollectionView.swift
@@ -0,0 +1,39 @@
+//
+//  CollectionView.swift
+//  CollectionUI
+//
+//  Created by opfic on 8/20/26.
+//
+
+import SwiftUI
+import UIKit
+
+// SwiftUI 화면과 UIKit collection controller를 연결한다.
+@MainActor
+public struct CollectionView<SectionIdentifier, ItemIdentifier>: UIViewControllerRepresentable
+where SectionIdentifier: Hashable & Sendable, ItemIdentifier: Hashable & Sendable {
+    private var configuration: CollectionViewConfiguration<SectionIdentifier, ItemIdentifier>
+
+    // 표시할 식별자 snapshot과 UIKit layout·cell provider를 지정해 container를 생성한다.
+    public init(
+        snapshot: CollectionRenderingSnapshot<SectionIdentifier, ItemIdentifier>,
+        layoutProvider: @escaping (SectionIdentifier, NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection,
+        cellProvider: @escaping (UICollectionView, IndexPath, ItemIdentifier) -> UICollectionViewCell?
+    ) {
+        configuration = CollectionViewConfiguration(
+            snapshot: snapshot,
+            layoutProvider: layoutProvider,
+            cellProvider: cellProvider
+        )
+    }
+
+    // UIKit collection controller를 생성한다.
+    public func makeUIViewController(context: Context) -> UIViewController {
+        CollectionViewController(configuration: configuration)
+    }
+
+    // 최신 rendering 입력과 provider를 UIKit controller에 반영한다.
+    public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        (uiViewController as? CollectionViewController)?.update(configuration: configuration)
+    }
+}
